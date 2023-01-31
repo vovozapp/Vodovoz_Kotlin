7cc07e27c0487f1b471d43388b01145f15ea43db
Vladislav Sotin <sotin.vladislav@mail.ru>
2023-01-31 13:21:57 +0300

    bind home view holders

--- app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainAdapter.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainAdapter.kt
@@ -10,13 +10,21 @@ import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBanners
 import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBannersSliderViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.homebottominfo.HomeBottomInfo
 import com.vodovoz.app.ui.fragment.home.viewholders.homebottominfo.HomeBottomInfoViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homebrands.HomeBrands
 import com.vodovoz.app.ui.fragment.home.viewholders.homebrands.HomeBrandsSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homecomments.HomeComments
 import com.vodovoz.app.ui.fragment.home.viewholders.homecomments.HomeCommentsSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homecountries.HomeCountries
 import com.vodovoz.app.ui.fragment.home.viewholders.homecountries.HomeCountriesSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homehistories.HomeHistories
 import com.vodovoz.app.ui.fragment.home.viewholders.homehistories.HomeHistoriesSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homeorders.HomeOrders
 import com.vodovoz.app.ui.fragment.home.viewholders.homeorders.HomeOrdersSliderViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.homepopulars.HomePopularCategoriesSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homepopulars.HomePopulars
+import com.vodovoz.app.ui.fragment.home.viewholders.homeproducts.HomeProducts
 import com.vodovoz.app.ui.fragment.home.viewholders.homeproducts.HomeProductsSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homepromotions.HomePromotions
 import com.vodovoz.app.ui.fragment.home.viewholders.homepromotions.HomePromotionsSliderViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.hometriplenav.HomeTripleNavViewHolder
 
@@ -84,7 +92,33 @@ class HomeMainAdapter(
             is HomeBottomInfoViewHolder -> {
                 holder.bind()
             }
-
+            is HomeBrandsSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeBrands)
+            }
+            is HomeCommentsSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeComments)
+            }
+            is HomeCountriesSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeCountries)
+            }
+            is HomeHistoriesSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeHistories)
+            }
+            is HomeOrdersSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeOrders)
+            }
+            is HomePopularCategoriesSliderViewHolder -> {
+                holder.bind(getItem(position) as HomePopulars)
+            }
+            is HomeProductsSliderViewHolder -> {
+                holder.bind(getItem(position) as HomeProducts)
+            }
+            is HomePromotionsSliderViewHolder -> {
+                holder.bind(getItem(position) as HomePromotions)
+            }
+            is HomeTripleNavViewHolder -> {
+                holder.bind()
+            }
         }
     }
 }
