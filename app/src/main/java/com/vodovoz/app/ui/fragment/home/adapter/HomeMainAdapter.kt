e18f87ed8764d81a4afb481bf7a1b4e48b26d0b2
rk_sotin <^V7ote>
2023-02-08 13:01:03 +0300

    fix home main adapter

--- app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainAdapter.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainAdapter.kt
@@ -3,6 +3,8 @@ package com.vodovoz.app.ui.fragment.home.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AdapterListUpdateCallback
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.vodovoz.app.R
@@ -10,6 +12,8 @@ import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBanners
 import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBanners.Companion.BANNER_LARGE
 import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBanners.Companion.BANNER_SMALL
 import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.HomeBannersSliderViewHolder
+import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.inneradapter.HomeBannersInnerDiffUtilCallback
+import com.vodovoz.app.ui.fragment.home.viewholders.homebanners.inneradapter.HomeBannersInnerViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.homebottominfo.HomeBottomInfo
 import com.vodovoz.app.ui.fragment.home.viewholders.homebottominfo.HomeBottomInfoViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.homebrands.HomeBrands
@@ -29,13 +33,35 @@ import com.vodovoz.app.ui.fragment.home.viewholders.homeproducts.HomeProductsSli
 import com.vodovoz.app.ui.fragment.home.viewholders.homepromotions.HomePromotions
 import com.vodovoz.app.ui.fragment.home.viewholders.homepromotions.HomePromotionsSliderViewHolder
 import com.vodovoz.app.ui.fragment.home.viewholders.hometriplenav.HomeTripleNavViewHolder
+import com.vodovoz.app.ui.model.BannerUI
 
 class HomeMainAdapter(
     private val clickListener: HomeMainClickListener
-) : ListAdapter<Item, RecyclerView.ViewHolder>(HomeMainDiffUtilCallback()) {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val items = mutableListOf<Item>()
+
+    private val adapterListUpdateCallback = AdapterListUpdateCallback(this)
+
+
+    fun submitList(items: List<Item>) {
+        val diff = DiffUtil.calculateDiff(HomeMainDiffUtilCallback(this.items, items), false)
+        diff.dispatchUpdatesTo(adapterListUpdateCallback)
+
+        this.items.clear()
+        this.items.addAll(items)
+    }
 
     override fun getItemViewType(position: Int): Int {
-        return getItem(position).getItemViewType()
+        return items[position].getItemViewType()
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun getItem(position: Int) : Item? {
+        return items.getOrNull(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -92,34 +118,34 @@ class HomeMainAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is HomeBannersSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeBanners)
+                holder.bind(items[position] as HomeBanners)
             }
             is HomeBottomInfoViewHolder -> {
                 holder.bind()
             }
             is HomeBrandsSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeBrands)
+                holder.bind(items[position]  as HomeBrands)
             }
             is HomeCommentsSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeComments)
+                holder.bind(items[position]  as HomeComments)
             }
             is HomeCountriesSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeCountries)
+                holder.bind(items[position]  as HomeCountries)
             }
             is HomeHistoriesSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeHistories)
+                holder.bind(items[position]  as HomeHistories)
             }
             is HomeOrdersSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeOrders)
+                holder.bind(items[position]  as HomeOrders)
             }
             is HomePopularCategoriesSliderViewHolder -> {
-                holder.bind(getItem(position) as HomePopulars)
+                holder.bind(items[position]  as HomePopulars)
             }
             is HomeProductsSliderViewHolder -> {
-                holder.bind(getItem(position) as HomeProducts)
+                holder.bind(items[position]  as HomeProducts)
             }
             is HomePromotionsSliderViewHolder -> {
-                holder.bind(getItem(position) as HomePromotions)
+                holder.bind(items[position]  as HomePromotions)
             }
             is HomeTripleNavViewHolder -> {
                 holder.bind()
--- app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainDiffUtilCallback.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/home/adapter/HomeMainDiffUtilCallback.kt
@@ -2,13 +2,24 @@ package com.vodovoz.app.ui.fragment.home.adapter
 
 import androidx.recyclerview.widget.DiffUtil
 
-class HomeMainDiffUtilCallback : DiffUtil.ItemCallback<Item>() {
+class HomeMainDiffUtilCallback(
+    private val oldItems: List<Item>,
+    private val newItems: List<Item>
+): DiffUtil.Callback() {
 
-    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem.areItemsTheSame(newItem)
+    override fun getOldListSize(): Int {
+        return oldItems.size
     }
 
-    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem.areContentsTheSame(newItem)
+    override fun getNewListSize(): Int {
+        return newItems.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItems[oldItemPosition].areItemsTheSame(newItems[newItemPosition])
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItems[oldItemPosition].areContentsTheSame(newItems[newItemPosition])
     }
 }
