bfdef04797fec4f8cdcd33ee6ceaf9eaae7a8b28
rk_sotin <^V7ote>
2023-03-22 11:50:20 +0300

    fix detail tabs

--- app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailtabs/DetailTabsViewHolder.kt
+++ app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailtabs/DetailTabsViewHolder.kt
@@ -1,13 +1,15 @@
 package com.vodovoz.app.feature.productdetail.viewholders.detailtabs
 
 import android.view.View
+import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import com.vodovoz.app.R
 import com.vodovoz.app.common.content.itemadapter.ItemViewHolder
 import com.vodovoz.app.databinding.FragmentProductDetailsTabsBinding
-import com.vodovoz.app.util.extensions.debugLog
+import com.vodovoz.app.ui.adapter.ProductPropertyGroupsAdapter
 
 class DetailTabsViewHolder(
     view: View,
@@ -15,23 +17,50 @@ class DetailTabsViewHolder(
 ) : ItemViewHolder<DetailTabs>(view) {
 
     private val binding: FragmentProductDetailsTabsBinding = FragmentProductDetailsTabsBinding.bind(view)
+    private var isExpanded = false
+    private val space: Int by lazy { itemView.context.resources.getDimension(R.dimen.space_16).toInt() }
+    private val productPropertyGroupsAdapter: ProductPropertyGroupsAdapter by lazy { ProductPropertyGroupsAdapter(space) }
 
     override fun bind(item: DetailTabs) {
         super.bind(item)
         binding.tvConsumerInfo.text = item.productDetailUI.consumerInfo
 
-        val adapter = DetailTabsViewPagerAdapter(fragment, item.productDetailUI)
-        binding.viewPager.adapter = adapter
-        binding.aboutTabs.setupWithViewPager(binding.viewPager, listOf("ОПИСАНИЕ", "ХАРАКТЕРИСТИКИ"))
+        binding.tvAboutProduct.originalText = HtmlCompat.fromHtml(item.productDetailUI.detailText, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+        if (isExpanded) {
+            binding.tvAboutProduct.toggle()
+        }
+
+        if (productPropertyGroupsAdapter.viewMode == ProductPropertyGroupsAdapter.ViewMode.ALL) {
+            binding.allProperties.visibility = View.GONE
+        }
+        binding.allProperties.setOnClickListener {
+            productPropertyGroupsAdapter.updateViewMode(ProductPropertyGroupsAdapter.ViewMode.ALL)
+            binding.allProperties.visibility = View.GONE
+        }
+
+        binding.propertiesGroupRecycler.layoutManager = LinearLayoutManager(itemView.context)
+        binding.propertiesGroupRecycler.adapter = productPropertyGroupsAdapter.also {
+            it.propertiesGroupUIList = item.productDetailUI.propertiesGroupUIList
+        }
+
+        binding.aboutTabs.addOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when(tab?.position) {
+                        0 -> {
+                            binding.tvAboutProduct.isVisible = true
+                            binding.properties.isVisible = false
+                        }
+                        1 -> {
+                            binding.tvAboutProduct.isVisible = false
+                            binding.properties.isVisible = true
+                        }
+                    }
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            }
+        )
     }
 
-    private fun TabLayout.setupWithViewPager(viewPager: ViewPager2, labels: List<String>) {
-
-        if (labels.size != viewPager.adapter?.itemCount)
-            throw Exception("The size of list and the tab count should be equal!")
-
-        TabLayoutMediator(this, viewPager) { tab, position ->
-            tab.text = labels[position]
-        }.attach()
-    }
 }
