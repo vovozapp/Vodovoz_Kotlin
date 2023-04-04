7e2d227c682616080c2084172a09cdb4aeb82851
rk_sotin <^V7ote>
2023-04-04 13:33:54 +0300

    fix detail prices

--- app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailprices/DetailPricesViewHolder.kt
+++ app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailprices/DetailPricesViewHolder.kt
@@ -2,9 +2,11 @@ package com.vodovoz.app.feature.productdetail.viewholders.detailprices
 
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.vodovoz.app.R
 import com.vodovoz.app.common.content.itemadapter.ItemViewHolder
 import com.vodovoz.app.databinding.FragmentProductDetailsPricesBinding
 import com.vodovoz.app.feature.productdetail.viewholders.detailprices.inner.PricesFlowAdapter
+import com.vodovoz.app.ui.extensions.RecyclerViewExtensions.addMarginDecoration
 
 class DetailPricesViewHolder(
     view: View
@@ -12,6 +14,7 @@ class DetailPricesViewHolder(
 
     private val binding: FragmentProductDetailsPricesBinding = FragmentProductDetailsPricesBinding.bind(view)
     private val adapter: PricesFlowAdapter = PricesFlowAdapter()
+    private val space = itemView.resources.getDimension(R.dimen.space_16).toInt()
 
     init {
         binding.rvPrices.layoutManager =
@@ -19,6 +22,14 @@ class DetailPricesViewHolder(
 
         binding.rvPrices.adapter = adapter
 
+        binding.rvPrices.addMarginDecoration {  rect, vie, parent, _ ->
+            if (parent.getChildAdapterPosition(vie) == 0) {
+                rect.left = space/1
+            }
+            rect.right = space/2
+            rect.bottom = space/4
+            rect.top = space/4
+        }
     }
 
     override fun bind(item: DetailPrices) {
