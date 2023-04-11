e255f4d2dadc205466d9ba1d1e8f800c47f0dee1
rk_sotin <^V7ote>
2023-04-11 15:16:31 +0300

    add popup flow

---
+++ app/src/main/java/com/vodovoz/app/feature/home/popup/NewsClickListener.kt
@@ -0,0 +1,8 @@
+package com.vodovoz.app.feature.home.popup
+
+import com.vodovoz.app.data.model.common.ActionEntity
+
+interface NewsClickListener {
+
+    fun onClick(actionEntity: ActionEntity)
+}
