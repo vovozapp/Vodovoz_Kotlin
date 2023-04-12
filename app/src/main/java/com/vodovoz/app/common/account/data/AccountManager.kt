0ec002d364c9717c8e2af9999e9da62e803af6cd
rk_sotin <^V7ote>
2023-04-12 16:55:47 +0300

    fix account manager

--- app/src/main/java/com/vodovoz/app/common/account/data/AccountManager.kt
+++ app/src/main/java/com/vodovoz/app/common/account/data/AccountManager.kt
@@ -25,10 +25,40 @@ class AccountManager @Inject constructor(
         sharedPrefs.edit().remove(USER_ID).apply()
     }
 
+    fun removeUserSettings() {
+        sharedPrefs.edit().remove(EMAIL).apply()
+        sharedPrefs.edit().remove(PASSWORD).apply()
+    }
+
+    fun fetchUserSettings() : UserSettings {
+        val email = sharedPrefs.getString(EMAIL, "") ?: ""
+        val password = sharedPrefs.getString(PASSWORD, "") ?: ""
+        return UserSettings(email, password)
+    }
+
+    fun updateLastLoginSetting(settings: UserSettings) {
+        with(sharedPrefs.edit()) {
+            putString(EMAIL, settings.email).apply()
+            putString(PASSWORD, settings.password).apply()
+        }
+    }
+
+    fun clearData() {
+        removeUserId()
+        removeUserSettings()
+    }
+
     fun isAlreadyLogin() = fetchUserId() != null
+
+    data class UserSettings(
+        val email: String,
+        val password: String
+    )
 
     companion object {
         private const val USER_ID = "USER_ID"
+        private const val EMAIL = "EMAIL"
+        private const val PASSWORD = "PASSWORD"
     }
 
 }
