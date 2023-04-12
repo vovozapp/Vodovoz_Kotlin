42fbfdeac1087e172a1cdcdaf65bfe6f612f82a2
rk_sotin <^V7ote>
2023-04-12 14:43:35 +0300

    create profile flow controller

--- app/src/main/java/com/vodovoz/app/feature/profile/ProfileFlowController.kt
+++ app/src/main/java/com/vodovoz/app/feature/profile/ProfileFlowController.kt
@@ -1,4 +1,61 @@
 package com.vodovoz.app.feature.profile
 
-class ProfileFlowController {
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.vodovoz.app.common.cart.CartManager
+import com.vodovoz.app.common.content.itemadapter.Item
+import com.vodovoz.app.common.like.LikeManager
+import com.vodovoz.app.feature.home.HomeFlowViewModel
+import com.vodovoz.app.feature.home.adapter.HomeMainAdapter
+import com.vodovoz.app.feature.home.adapter.HomeMainClickListener
+import com.vodovoz.app.feature.home.viewholders.homeorders.inneradapter.HomeOrdersSliderClickListener
+import com.vodovoz.app.feature.home.viewholders.homeproducts.ProductsShowAllListener
+import com.vodovoz.app.feature.home.viewholders.homepromotions.PromotionsClickListener
+import com.vodovoz.app.feature.productlist.adapter.ProductsClickListener
+import com.vodovoz.app.feature.profile.adapter.ProfileFlowAdapter
+import com.vodovoz.app.feature.profile.adapter.ProfileFlowClickListener
+
+class ProfileFlowController(
+    private val viewModel: ProfileFlowViewModel,
+    cartManager: CartManager,
+    likeManager: LikeManager,
+    listener: ProfileFlowClickListener,
+    productsShowAllListener: ProductsShowAllListener,
+    productsClickListener: ProductsClickListener,
+    homeOrdersSliderClickListener: HomeOrdersSliderClickListener
+) {
+
+    private val profileFlowAdatper = ProfileFlowAdapter(
+        listener,
+        cartManager,
+        likeManager,
+        productsShowAllListener,
+        productsClickListener,
+        homeOrdersSliderClickListener
+    )
+
+    fun bind(recyclerView: RecyclerView, refresh: SwipeRefreshLayout) {
+        initList(recyclerView)
+        bindRefresh(refresh)
+    }
+
+    fun submitList(list: List<Item>) {
+        profileFlowAdatper.submitList(list)
+    }
+
+    private fun initList(recyclerView: RecyclerView) {
+        with(recyclerView) {
+            adapter = profileFlowAdatper
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun bindRefresh(refresh: SwipeRefreshLayout) {
+
+        refresh.setOnRefreshListener {
+            viewModel.refresh()
+            refresh.isRefreshing = false
+        }
+    }
 }
