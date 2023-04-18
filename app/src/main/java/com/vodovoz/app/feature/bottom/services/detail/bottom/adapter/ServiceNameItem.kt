7ada95f7e4656b9df37ba22484e04e9354ee8b94
rk_sotin <^V7ote>
2023-04-18 11:04:39 +0300

    fix serv name item

--- app/src/main/java/com/vodovoz/app/feature/bottom/services/detail/bottom/adapter/ServiceNameItem.kt
+++ app/src/main/java/com/vodovoz/app/feature/bottom/services/detail/bottom/adapter/ServiceNameItem.kt
@@ -1,13 +1,16 @@
 package com.vodovoz.app.feature.bottom.services.detail.bottom.adapter
 
+import android.os.Parcelable
 import com.vodovoz.app.R
 import com.vodovoz.app.common.content.itemadapter.Item
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ServiceNameItem(
     val name: String,
     val type: String,
     val isSelected: Boolean = false
-) : Item {
+) : Item, Parcelable {
 
     override fun getItemViewType(): Int {
         return R.layout.view_holder_service_name
