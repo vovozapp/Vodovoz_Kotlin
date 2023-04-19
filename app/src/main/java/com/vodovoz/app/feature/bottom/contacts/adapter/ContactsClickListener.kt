af57830caea467c41624eefad43ce759166b88d4
rk_sotin <^V7ote>
2023-04-19 12:29:58 +0300

    add contacts us click listener

--- app/src/main/java/com/vodovoz/app/feature/bottom/contacts/adapter/ContactsClickListener.kt
+++ app/src/main/java/com/vodovoz/app/feature/bottom/contacts/adapter/ContactsClickListener.kt
@@ -1,4 +1,14 @@
 package com.vodovoz.app.feature.bottom.contacts.adapter
 
+import com.vodovoz.app.ui.model.ChatUI
+import com.vodovoz.app.ui.model.EmailUI
+import com.vodovoz.app.ui.model.PhoneUI
+
 interface ContactsClickListener {
+
+    fun onPhoneSelect(item: PhoneUI)
+    fun onChatIconSelect(item: ChatUI)
+
+    fun onEmailSelect(item: EmailUI)
+
 }
--- app/src/main/java/com/vodovoz/app/ui/model/ContactsUI.kt
+++ app/src/main/java/com/vodovoz/app/ui/model/ContactsUI.kt
@@ -1,5 +1,8 @@
 package com.vodovoz.app.ui.model
 
+import com.vodovoz.app.R
+import com.vodovoz.app.common.content.itemadapter.Item
+
 data class ContactsBundleUI(
     val title: String,
     val phoneUIList: List<PhoneUI>,
@@ -10,22 +13,70 @@ data class ContactsBundleUI(
 data class ChatsBundleUI(
     val name: String,
     val chatUIList: List<ChatUI>
-)
+) : Item {
+
+    override fun getItemViewType(): Int {
+        return R.layout.view_holder_contact_chats_bundle
+    }
+
+    override fun areItemsTheSame(item: Item): Boolean {
+        if (item !is ChatsBundleUI) return false
+
+        return this == item
+    }
+
+}
 
 data class ChatUI(
     val icon: String,
     val action: String,
     val type: String
-)
+) : Item {
+
+    override fun getItemViewType(): Int {
+        return R.layout.view_holder_contact_chat_icon
+    }
+
+    override fun areItemsTheSame(item: Item): Boolean {
+        if (item !is ChatUI) return false
+
+        return this == item
+    }
+
+}
 
 data class PhoneUI(
     val name: String,
     val value: String,
     val type: String
-)
+) : Item {
+
+    override fun getItemViewType(): Int {
+        return R.layout.view_holder_contact_phone
+    }
+
+    override fun areItemsTheSame(item: Item): Boolean {
+        if (item !is PhoneUI) return false
+
+        return this == item
+    }
+
+}
 
 data class EmailUI(
     val name: String,
     val value: String,
     val type: String
-)
+) : Item {
+
+    override fun getItemViewType(): Int {
+        return R.layout.view_holder_contact_email
+    }
+
+    override fun areItemsTheSame(item: Item): Boolean {
+        if (item !is EmailUI) return false
+
+        return this == item
+    }
+
+}
