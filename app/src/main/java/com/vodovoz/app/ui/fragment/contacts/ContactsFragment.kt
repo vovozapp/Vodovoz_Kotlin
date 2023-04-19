04cd595e54975841db131302265da584422bc911
rk_sotin <^V7ote>
2023-04-19 13:18:29 +0300

    fix contacts

--- app/src/main/java/com/vodovoz/app/ui/fragment/contacts/ContactsFragment.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/contacts/ContactsFragment.kt
@@ -5,27 +5,181 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.snackbar.Snackbar
 import com.vodovoz.app.R
+import com.vodovoz.app.common.content.BaseFragment
 import com.vodovoz.app.data.model.common.*
 import com.vodovoz.app.databinding.FragmentContactsBinding
+import com.vodovoz.app.databinding.FragmentContactsFlowBinding
+import com.vodovoz.app.feature.bottom.contacts.ContactsFlowViewModel
+import com.vodovoz.app.feature.bottom.contacts.adapter.ContactsClickListener
+import com.vodovoz.app.feature.bottom.contacts.adapter.controller.EmailController
+import com.vodovoz.app.feature.bottom.contacts.adapter.controller.PhoneController
 import com.vodovoz.app.ui.adapter.EmailContactsAdapter
 import com.vodovoz.app.ui.adapter.PhoneContactsAdapter
 import com.vodovoz.app.ui.base.ViewState
 import com.vodovoz.app.ui.base.ViewStateBaseFragment
 import com.vodovoz.app.ui.base.VodovozApplication
 import com.vodovoz.app.ui.extensions.ScrollViewExtensions.setScrollElevation
+import com.vodovoz.app.ui.model.ChatUI
+import com.vodovoz.app.ui.model.EmailUI
+import com.vodovoz.app.ui.model.PhoneUI
+import com.vodovoz.app.util.extensions.snack
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class ContactsFragment : BaseFragment() {
+
+    override fun layout(): Int = R.layout.fragment_contacts_flow
+
+    private val viewModel: ContactsFlowViewModel by viewModels()
+
+    private val binding: FragmentContactsFlowBinding by viewBinding {
+        FragmentContactsFlowBinding.bind(
+            contentView
+        )
+    }
+
+    private val emailController = EmailController(getContactsClickListener())
+    private val phoneController = PhoneController(getContactsClickListener())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.fetchContacts()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        phoneController.bind(binding.phonesRecycler)
+        emailController.bind(binding.emailsRecycler)
+
+        initToolbar("Связаться с нами")
+        initWriteUsButton()
+        observeUiState()
+        observeEvents()
+    }
+
+    private fun observeEvents() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.observeEvent()
+                .collect {
+                    when (it) {
+                        is ContactsFlowViewModel.ContactsEvents.SendEmailSuccess -> {
+                            requireActivity().snack(it.message)
+                        }
+                        is ContactsFlowViewModel.ContactsEvents.SendEmailError -> {
+                            requireActivity().snack(it.message)
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun observeUiState() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.observeUiState()
+                .collect { state ->
+
+                    if (state.loadingPage) {
+                        showLoader()
+                    } else {
+                        hideLoader()
+                    }
+
+                    if (state.data.item != null) {
+
+                        emailController.submitList(state.data.item.emailUIList)
+                        val chatBundle = state.data.item.chatsBundleUI
+                        val mappedList = if (chatBundle != null) {
+                            state.data.item.phoneUIList + state.data.item.chatsBundleUI
+                        } else {
+                            state.data.item.phoneUIList
+                        }
+
+                        phoneController.submitList(mappedList)
+                    }
+
+                    showError(state.error)
+
+                }
+        }
+    }
+
+    private fun initWriteUsButton() {
+        binding.writeUsContainer.submit.setOnClickListener { writeUs() }
+    }
+
+    private fun writeUs() {
+        if (!binding.writeUsContainer.personalDataSwitch.isChecked) {
+            requireActivity().snack("Подтвердите согласие на обработку данных!")
+            return
+        }
+        val name = binding.writeUsContainer.name.text.toString()
+        val phone = binding.writeUsContainer.phone.text.toString()
+        val email = binding.writeUsContainer.email.text.toString()
+        val descriptions = binding.writeUsContainer.description.text.toString()
+
+        if (name.isEmpty() || phone.isEmpty() || email.isEmpty() || descriptions.isEmpty()) {
+            requireActivity().snack("Заполните все поля")
+            return
+        }
+
+        viewModel.sendMail(name, phone, email, descriptions)
+    }
+
+    private fun getContactsClickListener() : ContactsClickListener {
+        return object : ContactsClickListener {
+            override fun onPhoneSelect(item: PhoneUI) {
+                when(item.type) {
+                    WHATSAPP_TYPE -> {
+                        val uri = Uri.parse("https://api.whatsapp.com/send?phone=${item.value}")
+                        val sendIntent = Intent(Intent.ACTION_VIEW, uri)
+                        startActivity(sendIntent)
+                    }
+                    PHONE_TYPE -> {
+                        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", item.value, null))
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onChatIconSelect(item: ChatUI) {
+                when(item.type) {
+                    WHATSAPP_TYPE -> {
+                        val uri = Uri.parse("https://api.whatsapp.com/send?phone=${item.action}")
+                        val sendIntent = Intent(Intent.ACTION_VIEW, uri)
+                        startActivity(sendIntent)
+                    }
+                    VIBER_TYPE -> {
+                    }
+                    TELEGRAM_TYPE -> {}
+                    CHAT_TYPE -> {}
+                }
+            }
+
+            override fun onEmailSelect(item: EmailUI) {
+                val emailIntent = Intent(Intent.ACTION_SEND)
+                emailIntent.type = "plain/text"
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(item.value))
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."))
+            }
+        }
+    }
+}
+/*
 @AndroidEntryPoint
 class ContactsFragment : ViewStateBaseFragment() {
 
@@ -167,4 +321,4 @@ class ContactsFragment : ViewStateBaseFragment() {
         }
     }
 
-}
+}*/
