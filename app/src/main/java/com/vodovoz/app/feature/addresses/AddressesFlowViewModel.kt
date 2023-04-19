70ff68cabc5f0ef041362c88838696c873d3a166
rk_sotin <^V7ote>
2023-04-19 13:52:23 +0300

    add flows

---
+++ app/src/main/java/com/vodovoz/app/feature/addresses/AddressesFlowViewModel.kt
@@ -0,0 +1,4 @@
+package com.vodovoz.app.feature.addresses
+
+class AddressesFlowViewModel {
+}
---
+++ app/src/main/java/com/vodovoz/app/feature/addresses/adapter/viewholders/AddressTitleFlowViewHolder.kt
@@ -0,0 +1,4 @@
+package com.vodovoz.app.feature.addresses.adapter.viewholders
+
+class AddressTitleFlowViewHolder {
+}
