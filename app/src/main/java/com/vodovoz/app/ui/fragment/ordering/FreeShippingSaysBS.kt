88e8934126cb701b7ebffd7ac46cd11ad83c41b0
rk_sotin <^V7ote>
2023-04-21 15:21:48 +0300

    fix bs

--- app/src/main/java/com/vodovoz/app/ui/fragment/ordering/FreeShippingSaysBS.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/ordering/FreeShippingSaysBS.kt
@@ -2,11 +2,41 @@ package com.vodovoz.app.ui.fragment.ordering
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
+import androidx.navigation.fragment.navArgs
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.vodovoz.app.R
+import com.vodovoz.app.common.content.BaseBottomSheetFragment
 import com.vodovoz.app.databinding.BsFreeShippingDaysBinding
+import com.vodovoz.app.util.extensions.fromHtml
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class FreeShippingSaysBS : BaseBottomSheetFragment() {
+
+    override fun layout(): Int {
+        return R.layout.bs_free_shipping_days
+    }
+
+    private val binding: BsFreeShippingDaysBinding by viewBinding {
+        BsFreeShippingDaysBinding.bind(contentView)
+    }
+
+    private val args: FreeShippingSaysBSArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.incHeader.imgClose.setOnClickListener { dismiss() }
+        binding.incHeader.tvTitle.text = args.title
+        binding.tvContent.text = args.info.fromHtml()
+    }
+}
+
+/*
 class FreeShippingSaysBS : BottomSheetDialogFragment() {
 
     override fun onCreateView(
@@ -26,4 +56,4 @@ class FreeShippingSaysBS : BottomSheetDialogFragment() {
         )
     }.root
 
-}
+}*/
--- app/src/main/java/com/vodovoz/app/ui/fragment/ordering/PayMethodSelectionBS.kt
+++ app/src/main/java/com/vodovoz/app/ui/fragment/ordering/PayMethodSelectionBS.kt
@@ -2,15 +2,62 @@ package com.vodovoz.app.ui.fragment.ordering
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.vodovoz.app.R
+import com.vodovoz.app.common.content.BaseBottomSheetFragment
 import com.vodovoz.app.databinding.BsSelectionPayMethodBinding
+import com.vodovoz.app.feature.cart.ordering.intervals.adapter.IntervalsClickListener
+import com.vodovoz.app.feature.cart.ordering.intervals.adapter.IntervalsController
 import com.vodovoz.app.ui.adapter.PayMethodsAdapter
 import com.vodovoz.app.ui.extensions.RecyclerViewExtensions.addMarginDecoration
+import com.vodovoz.app.ui.model.PayMethodUI
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class PayMethodSelectionBS : BaseBottomSheetFragment() {
+
+    override fun layout(): Int {
+        return R.layout.bs_selection_pay_method
+    }
+
+    private val binding: BsSelectionPayMethodBinding by viewBinding {
+        BsSelectionPayMethodBinding.bind(contentView)
+    }
+
+    private val args: PayMethodSelectionBSArgs by navArgs()
+
+    private val intervalsController by lazy {
+        IntervalsController(getIntervalsClickListener(), requireContext())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val list = args.payMethodList.toList().map {
+            it.copy(
+                isSelected = it.id == args.selectedPayMethod
+            )
+        }
+
+        intervalsController.bind(binding.rvPayMethods, list)
+    }
+
+    private fun getIntervalsClickListener() : IntervalsClickListener {
+        return object: IntervalsClickListener {
+            override fun onPayMethodClick(item: PayMethodUI) {
+                findNavController().previousBackStackEntry?.savedStateHandle?.set(OrderingFragment.SELECTED_PAY_METHOD, item.id)
+                dismiss()
+            }
+        }
+    }
+}
+/*
 class PayMethodSelectionBS : BottomSheetDialogFragment() {
 
     private val payMethodsAdapter = PayMethodsAdapter()
@@ -41,4 +88,4 @@ class PayMethodSelectionBS : BottomSheetDialogFragment() {
         }
     }.root
 
-}
+}*/
