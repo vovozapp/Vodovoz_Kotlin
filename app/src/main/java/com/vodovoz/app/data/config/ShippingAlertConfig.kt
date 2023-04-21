c18544d75b7743ef3e64abea5751ec6d98f38d48
rk_sotin <^V7ote>
2023-04-21 10:43:39 +0300

    fix ordering

--- app/src/main/java/com/vodovoz/app/data/config/ShippingAlertConfig.kt
+++ app/src/main/java/com/vodovoz/app/data/config/ShippingAlertConfig.kt
@@ -1,6 +1,7 @@
 package com.vodovoz.app.data.config
 
 import com.vodovoz.app.data.model.common.ShippingAlertEntity
+import com.vodovoz.app.mapper.ShippingAlertMapper.mapToUI
 
 object ShippingAlertConfig {
 
@@ -22,5 +23,24 @@ object ShippingAlertConfig {
             "За 120 минут"
         )
     )
+
+    val shippingAlertEntityListUI = listOf<ShippingAlertEntity>(
+        ShippingAlertEntity(
+            id = 1,
+            "За 30 минут"
+        ),
+        ShippingAlertEntity(
+            id = 2,
+            "За 60 минут"
+        ),
+        ShippingAlertEntity(
+            id = 3,
+            "За 90 минут"
+        ),
+        ShippingAlertEntity(
+            id = 4,
+            "За 120 минут"
+        )
+    ).mapToUI()
 
 }
--- app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowViewModel.kt
+++ app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowViewModel.kt
@@ -1,14 +1,17 @@
 package com.vodovoz.app.feature.cart.ordering
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import com.vodovoz.app.common.content.Event
 import com.vodovoz.app.common.content.PagingContractViewModel
 import com.vodovoz.app.common.content.State
 import com.vodovoz.app.common.content.itemadapter.Item
 import com.vodovoz.app.data.MainRepository
 import com.vodovoz.app.data.config.ShippingAlertConfig
-import com.vodovoz.app.data.model.common.ShippingAlertEntity
+import com.vodovoz.app.ui.model.ShippingAlertUI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,11 +22,16 @@ class OrderingFlowViewModel @Inject constructor(
     OrderingState()
 ) {
 
+    fun fetchFreeShippingDaysInfo() {
+        viewModelScope.launch {
+            flow { emit(repository.fetchFreeShippingDaysInfoResponse()) }
+        }
+    }
 
 
     data class OrderingState(
         val item: Item? = null,
-        val shippingAlertList: List<ShippingAlertEntity> = ShippingAlertConfig.shippingAlertEntityList
+        val shippingAlertList: List<ShippingAlertUI> = ShippingAlertConfig.shippingAlertEntityListUI
     ) : State
 
     sealed class OrderingEvents : Event {
