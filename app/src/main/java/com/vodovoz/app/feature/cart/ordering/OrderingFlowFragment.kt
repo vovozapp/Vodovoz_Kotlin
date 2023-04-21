4d620605e2e7d12742f9b6f2c4202f95641e9b01
rk_sotin <^V7ote>
2023-04-21 09:20:53 +0300

    add ordering flow

--- app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowFragment.kt
+++ app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowFragment.kt
@@ -1,4 +1,40 @@
 package com.vodovoz.app.feature.cart.ordering
 
-class OrderingFlowFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.vodovoz.app.R
+import com.vodovoz.app.common.content.BaseFragment
+import com.vodovoz.app.databinding.FragmentOrderingFlowBinding
+import com.vodovoz.app.ui.fragment.ordering.OrderingFragmentArgs
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OrderingFlowFragment : BaseFragment() {
+
+    companion object {
+        const val SELECTED_PAY_METHOD = "SELECTED_PAY_METHOD"
+        const val SELECTED_SHIPPING_INTERVAL = "SELECTED_SHIPPING_INTERVAL"
+        const val SELECTED_SHIPPING_ALERT = "SELECTED_SHIPPING_ALERT"
+    }
+
+    override fun layout(): Int = R.layout.fragment_ordering_flow
+
+    private val binding: FragmentOrderingFlowBinding by viewBinding {
+        FragmentOrderingFlowBinding.bind(contentView)
+    }
+
+    private val args: OrderingFragmentArgs by navArgs()
+
+    private val viewModel: OrderingFlowViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initToolbar(getString(R.string.ordering_title_text))
+
+
+    }
 }
--- app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowViewModel.kt
+++ app/src/main/java/com/vodovoz/app/feature/cart/ordering/OrderingFlowViewModel.kt
@@ -1,4 +1,29 @@
 package com.vodovoz.app.feature.cart.ordering
 
-class OrderingFlowViewModel {
+import androidx.lifecycle.SavedStateHandle
+import com.vodovoz.app.common.content.Event
+import com.vodovoz.app.common.content.PagingContractViewModel
+import com.vodovoz.app.common.content.State
+import com.vodovoz.app.common.content.itemadapter.Item
+import com.vodovoz.app.data.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderingFlowViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val repository: MainRepository
+) : PagingContractViewModel<OrderingFlowViewModel.OrderingState, OrderingFlowViewModel.OrderingEvents>(
+    OrderingState()
+) {
+
+
+
+    data class OrderingState(
+        val item: Item? = null
+    ) : State
+
+    sealed class OrderingEvents : Event {
+
+    }
 }
