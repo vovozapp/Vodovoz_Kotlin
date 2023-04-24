953b31988305379c3c83703b0eec695820dbf78d
rk_sotin <^V7ote>
2023-04-24 16:30:06 +0300

    fix catalog

--- app/src/main/java/com/vodovoz/app/util/FilterBuilderExtensions.kt
+++ app/src/main/java/com/vodovoz/app/util/FilterBuilderExtensions.kt
@@ -1,28 +1,43 @@
 package com.vodovoz.app.util
 
 import com.vodovoz.app.ui.model.FilterUI
+import com.vodovoz.app.util.extensions.debugLog
 
 object FilterBuilderExtensions {
 
-    fun List<FilterUI>?.buildFilterQuery() = StringBuilder().also { builder ->
-        this?.let {
-            forEach { builder.append(it.code).append(",") }
+    fun List<FilterUI>?.buildFilterQuery(): String {
+        return try {
+            StringBuilder().also { builder ->
+                this?.let {
+                    forEach { builder.append(it.code).append(",") }
+                }
+            }.toString()
+        } catch (t: Throwable) {
+            debugLog { "filter builder ext $t" }
+            ""
         }
-    }.toString()
+    }
 
-    fun List<FilterUI>?.buildFilterValueQuery() = StringBuilder().also { builder ->
-        this?.let {
-            forEach { filter ->
-                builder.append(filter.code).append("@")
-                filter.filterValueList.forEach { filterValue ->
-                    builder.append(filterValue.id)
-                    if (filterValue != filter.filterValueList.last()) {
-                        builder.append(",")
+    fun List<FilterUI>?.buildFilterValueQuery(): String {
+        return try {
+            StringBuilder().also { builder ->
+                this?.let {
+                    forEach { filter ->
+                        builder.append(filter.code).append("@")
+                        filter.filterValueList.forEach { filterValue ->
+                            builder.append(filterValue.id)
+                            if (filterValue != filter.filterValueList.last()) {
+                                builder.append(",")
+                            }
+                        }
+                        builder.append(";")
                     }
                 }
-                builder.append(";")
-            }
+            }.toString()
+        } catch (t: Throwable) {
+            debugLog { "filter builder ext $t" }
+            ""
         }
-    }.toString()
+    }
 
 }
