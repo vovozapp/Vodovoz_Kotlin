9218069d655104e2ce51339731fe11a162d5317a
rk_sotin <^V7ote>
2023-05-11 11:40:22 +0300

    add curl

---
+++ app/send_chat_message.sh
@@ -0,0 +1,8 @@
+curl -X POST -H "Authorization: key=AAAAt7juqLs:APA91bFV2CEwTi1a9CzTwfHBpGQqVlGwTGjufflwBf7Iyty9qERY45IQzDurQdx0DI0uqnVM6IuWgHrZ7XBzb-a42RNo8Ouapoyk7UkFfxZWD6oQdmLr55KKPF_lwP9nF-TKvgAPW1aX" -H "Content-Type: application/json" -d '{
+    "to":"cK147myJTDmdyN0CF8XiZa:APA91bEXXuS98NEQx28XCQ6csutf2aX1tAebxJ5jctzcwODAfHZHR6RMNtnGIk_PCGBdRRBOmalit0U2dIJVmb2jmCijl1oGFjs81nYUzAK-cb0y8fiCmsjGYroH4J83gUnHSFrynQvf",
+    "data": {
+      "imageUrl":"new",
+      "title":"new",
+      "body":"fafasafsasffa"
+     }
+}' https://fcm.googleapis.com/fcm/send -v -i
