b7c2e5618fab90005d18f0ea10945dafb8dd2b34
rk_sotin <^V7ote>
2023-05-16 11:50:00 +0300

    fix tab layouts

--- app/src/main/java/com/vodovoz/app/feature/home/viewholders/homeproducts/HomeProductsSliderViewHolder.kt
+++ app/src/main/java/com/vodovoz/app/feature/home/viewholders/homeproducts/HomeProductsSliderViewHolder.kt
@@ -1,19 +1,12 @@
 package com.vodovoz.app.feature.home.viewholders.homeproducts
 
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ahmadhamwi.tabsync.TabbedListMediator
-import com.google.android.material.tabs.TabLayout
-import com.vodovoz.app.R
 import com.vodovoz.app.common.cart.CartManager
-import com.vodovoz.app.databinding.FragmentSliderProductBinding
-import com.vodovoz.app.databinding.ViewCustomTabBinding
 import com.vodovoz.app.common.content.itemadapter.ItemViewHolder
 import com.vodovoz.app.common.like.LikeManager
+import com.vodovoz.app.databinding.FragmentSliderProductBinding
 import com.vodovoz.app.feature.home.viewholders.homeproducts.HomeProducts.Companion.BOTTOM_PROD
 import com.vodovoz.app.feature.home.viewholders.homeproducts.HomeProducts.Companion.DISCOUNT
 import com.vodovoz.app.feature.home.viewholders.homeproducts.HomeProducts.Companion.NOVELTIES
@@ -30,15 +23,7 @@ class HomeProductsSliderViewHolder(
 ) : ItemViewHolder<HomeProducts>(view) {
 
     private val binding: FragmentSliderProductBinding = FragmentSliderProductBinding.bind(view)
-    private val space: Int by lazy { itemView.resources.getDimension(R.dimen.space_16).toInt() }
     private val homeCategoriesAdapter = HomeCategoriesInnerAdapter(productsClickListener, cartManager, likeManager)
-    private val tab = TabbedListMediator(
-        binding.rvCategories,
-        binding.tlCategories,
-        listOf()
-    )
-
-    private var isAdded = false
 
     init {
         binding.rvCategories.layoutManager =
@@ -46,42 +31,8 @@ class HomeProductsSliderViewHolder(
 
         binding.rvCategories.adapter = homeCategoriesAdapter
 
-        binding.tlCategories.addOnTabSelectedListener(
-            object : TabLayout.OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab?) {
-                    tab?.view?.findViewById<TextView>(R.id.name)?.setTextColor(
-                        ContextCompat.getColor(itemView.context,
-                            R.color.bluePrimary)
-                    )
-                }
-                override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    tab?.view?.findViewById<TextView>(R.id.name)?.setTextColor(
-                        ContextCompat.getColor(itemView.context,
-                            R.color.text_black)
-                    )
-                }
-                override fun onTabReselected(tab: TabLayout.Tab?) {}
-            }
-        )
-    }
-
-    override fun detach() {
-        super.detach()
-        tab.detach()
-    }
-
-    override fun bind(item: HomeProducts) {
-        super.bind(item)
-
-        homeCategoriesAdapter.submitList(item.items)
-
-        initShowAllProductsButtons(item)
-        binding.tlCategories.removeAllTabs()
-        updateCategoryTabs(item)
-    }
-
-    private fun initShowAllProductsButtons(items: HomeProducts) {
         binding.tvShowAll.setOnClickListener {
+            val items = item ?: return@setOnClickListener
             items.items.first().id?.let { categoryId ->
                 when(items.productsType) {
                     DISCOUNT -> productsShowAllListener.showAllDiscountProducts(categoryId)
@@ -92,6 +43,13 @@ class HomeProductsSliderViewHolder(
                 }
             }
         }
+    }
+
+    override fun bind(item: HomeProducts) {
+        super.bind(item)
+
+        homeCategoriesAdapter.submitList(item.items)
+        updateCategoryTabs(item)
     }
 
     private fun updateCategoryTabs(items: HomeProducts) {
@@ -110,49 +68,17 @@ class HomeProductsSliderViewHolder(
                 binding.tlCategories.visibility = View.VISIBLE
                 binding.tvName.visibility = View.GONE
                 binding.tvShowAll.visibility = View.GONE
-                for (index in items.items.indices) {
-                    binding.tlCategories.newTab().apply {
-                        val customTabBinding = ViewCustomTabBinding.inflate(
-                            LayoutInflater.from(itemView.context), null, false
-                        )
 
-                        val marginEnd: Int
-                        val marginStart: Int
-                        val marginTop = space/4
-                        val marginBottom = space/4
-
-                        when (index) {
-                            0 -> {
-                                marginStart = space
-                                marginEnd = space/4
-                            }
-                            items.items.indices.last -> {
-                                marginEnd = space
-                                marginStart = space/2
-                            }
-                            else -> {
-                                marginEnd = space/4
-                                marginStart = space/2
-                            }
-                        }
-
-                        customTabBinding.card.layoutParams = RelativeLayout.LayoutParams(
-                            RelativeLayout.LayoutParams.WRAP_CONTENT,
-                            RelativeLayout.LayoutParams.WRAP_CONTENT
-                        ).apply {
-                            topMargin = marginTop
-                            bottomMargin = marginBottom
-                            leftMargin = marginStart
-                            rightMargin = marginEnd
-                        }
-
-                        customTabBinding.name.text = items.items[index].name
-                        customView = customTabBinding.root
-                        binding.tlCategories.addTab(this)
-                    }
+                binding.tlCategories.removeAllTabs()
+                items.items.forEach {
+                    binding.tlCategories.addTab(binding.tlCategories.newTab().setText(it.name))
                 }
-                tab.updateMediatorWithNewIndices(items.items.indices.toList())
-                if (!tab.isAttached()) tab.attach()
+
+                TabbedListMediator(
+                    binding.rvCategories,
+                    binding.tlCategories,
+                    items.items.indices.toList()
+                ).attach()
             }
         }
     }
