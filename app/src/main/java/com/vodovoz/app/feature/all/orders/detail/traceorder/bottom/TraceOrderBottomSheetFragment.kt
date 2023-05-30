1f5b7b251a94c53af668d317f9ab063e4480b6eb
rk_sotin <^V7ote>
2023-05-30 15:15:15 +0300

    fix

--- app/src/main/java/com/vodovoz/app/feature/all/orders/detail/traceorder/bottom/TraceOrderBottomSheetFragment.kt
+++ app/src/main/java/com/vodovoz/app/feature/all/orders/detail/traceorder/bottom/TraceOrderBottomSheetFragment.kt
@@ -1,13 +1,19 @@
 package com.vodovoz.app.feature.all.orders.detail.traceorder.bottom
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.vodovoz.app.R
 import com.vodovoz.app.common.content.BaseBottomSheetFragment
 import com.vodovoz.app.databinding.FragmentTraceOrderBottomBinding
 import com.vodovoz.app.feature.all.orders.detail.traceorder.TraceOrderViewModel
+import com.vodovoz.app.feature.bottom.contacts.ContactsFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,7 +29,48 @@ class TraceOrderBottomSheetFragment : BaseBottomSheetFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeUiState()
+        bindButtons()
+    }
 
+    private fun bindButtons() {
+        binding.callUsBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "+74959213434", null))
+            startActivity(intent)
+        }
+
+        binding.chatUsBtn.setOnClickListener {
+            findNavController().navigate(
+                TraceOrderBottomSheetFragmentDirections.actionToWebViewFragment(
+                "http://jivo.chat/mk31km1IlP",
+                "Чат"
+            ))
+        }
+    }
+
+    private fun observeUiState() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.observeUiState()
+                .collect {
+                    binding.driverNameTv.text = it.data.name ?: ""
+                    binding.carNumberTv.text = it.data.car ?: ""
+
+                    if (it.data.driverPointsEntity != null) {
+                        if (it.data.driverPointsEntity.DriverDirection == "TRUE") {
+                            binding.timeTv.isVisible = false
+                            binding.commentTv.isVisible = true
+                            binding.commentTv.text = "Водитель выехал и направляется к Вам."
+                        } else {
+                            binding.timeTv.isVisible = true
+                            binding.commentTv.isVisible = false
+                            binding.commentTv.text = "Ориентировочное время прибытия: ${it.data.driverPointsEntity.Priblizitelnoe_vremya}"
+                        }
+                    } else {
+                        binding.timeTv.isVisible = false
+                        binding.commentTv.isVisible = false
+                    }
+                }
+        }
     }
 
 }
