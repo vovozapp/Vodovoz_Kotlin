cc1412faa607233d5f37a5d82aeac1f3c729c662
rk_sotin <^V7ote>
2023-06-30 11:22:50 +0300

    fix

--- app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailservices/DetailServicesViewHolder.kt
+++ app/src/main/java/com/vodovoz/app/feature/productdetail/viewholders/detailservices/DetailServicesViewHolder.kt
@@ -3,6 +3,7 @@ package com.vodovoz.app.feature.productdetail.viewholders.detailservices
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vodovoz.app.R
 import com.vodovoz.app.common.content.itemadapter.ItemViewHolder
@@ -20,7 +21,7 @@ class DetailServicesViewHolder(
     private val space: Int by lazy { itemView.context.resources.getDimension(R.dimen.space_16).toInt() }
 
     init {
-        binding.rvServices.layoutManager = GridLayoutManager(itemView.context, 2)
+        binding.rvServices.layoutManager = GridLayoutManager(itemView.context, 2, GridLayoutManager.VERTICAL, false)
 
         binding.rvServices.adapter = adapter
 
@@ -40,8 +41,8 @@ class DetailServicesViewHolder(
                             left = space/4
                             right = space
                         }
-                        top = space
-                        bottom = space
+                        top = space / 2
+                        bottom = space / 2
                     }
                 }
             }
