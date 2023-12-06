58cac153f90336a3fc9901e9aff0d050332d6375
kmeshkov <metallicaisgod@yandex.ru>
2023-12-06 17:39:04 +0300

    lottie animation

--- app/src/main/java/com/vodovoz/app/ui/base/VodovozApplication.kt
+++ app/src/main/java/com/vodovoz/app/ui/base/VodovozApplication.kt
@@ -7,15 +7,17 @@ import com.vodovoz.app.core.network.ApiConfig
 import com.vodovoz.app.util.Keys
 import com.vodovoz.app.util.Keys.YANDEX_METRICA_KEY
 import com.vodovoz.app.util.SplashFileConfig
+import com.vodovoz.app.util.SplashFileConfig.downloadSplashFile
 import com.vodovoz.app.util.extensions.debugLog
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import timber.log.Timber
-import java.io.FileOutputStream
-import java.net.URL
 
 @HiltAndroidApp
 class VodovozApplication : Application() {
@@ -30,9 +32,9 @@ class VodovozApplication : Application() {
 
         val file = SplashFileConfig.getSplashFile(this)
         if (!file.exists()) {
-            debugLog { "file is not exist" }
+            debugLog { "splash file is not exist" }
             coroutineScope.launch(Dispatchers.IO) {
-                download("https://vodovoz.ru/images/zastavka/zastavkamobil.json")
+                downloadSplashFile(this@VodovozApplication)
             }
         }
     }
@@ -40,20 +42,6 @@ class VodovozApplication : Application() {
     override fun onTerminate() {
         coroutineScope.cancel()
         super.onTerminate()
-    }
-
-    private suspend fun download(link: String) {
-        withContext(Dispatchers.IO) {
-            URL(link).openStream()
-        }.use { input ->
-            val file = SplashFileConfig.getSplashFile(this)
-            if (!file.exists()) {
-                file.createNewFile()
-            }
-            FileOutputStream(file).use { output ->
-                input.copyTo(output)
-            }
-        }
     }
 
     private fun initYandexMetrica() {
--- app/src/main/java/com/vodovoz/app/util/SplashFileConfig.kt
+++ app/src/main/java/com/vodovoz/app/util/SplashFileConfig.kt
@@ -1,13 +1,32 @@
 package com.vodovoz.app.util
 
 import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
 
 object SplashFileConfig {
 
     private const val FILE_NAME = "splash.json"
+    private const val DAFAULT_LINK = "https://vodovoz.ru/images/zastavka/zastavkamobil.json"
 
     fun getSplashFile(context: Context) : File {
         return File(context.filesDir, FILE_NAME)
+    }
+
+    suspend fun downloadSplashFile(context: Context, link: String = DAFAULT_LINK) {
+        withContext(Dispatchers.IO) {
+            URL(link).openStream()
+        }.use { input ->
+            val file = getSplashFile(context)
+            if (!file.exists()) {
+                file.createNewFile()
+            }
+            FileOutputStream(file).use { output ->
+                input.copyTo(output)
+            }
+        }
     }
 }
