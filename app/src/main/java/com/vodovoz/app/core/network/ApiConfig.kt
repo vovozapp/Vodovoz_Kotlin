ce37f2754229a1e038816656cf325ce32aa4c2f6
kmeshkov <metallicaisgod@yandex.ru>
2024-05-24 18:19:56 +0300

    ref

--- app/src/main/java/com/vodovoz/app/core/network/ApiConfig.kt
+++ app/src/main/java/com/vodovoz/app/core/network/ApiConfig.kt
@@ -15,7 +15,7 @@ object ApiConfig {
     var ABOUT_SHOP_URL = VODOVOZ_URL + "newmobile/informatsiya/omagazine.php"
     var ABOUT_PAY_URL = VODOVOZ_URL + "newmobile/informatsiya/oplata.php"
     var ABOUT_DELIVERY_URL = VODOVOZ_URL + "newmobile/informatsiya/dosytavka.php"
-    val PERSONAL_DATA_URL = "https://vodovoz.ru/delivery_files/informaciya/personalnie-dannie.php"
+    const val PERSONAL_DATA_URL = "https://vodovoz.ru/delivery_files/informaciya/personalnie-dannie.php"
 
     const val AMOUNT_CONTROLLER_TIMER = 1500L
 
